60a48da4736d3bacdbff885a2128723f653caec8
TungH <tungh@ichiisoft.com>
2023-08-29 22:37:42 +0700

    flutter deploy web

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hoang Tung</h1>');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public-flutter')));
+app.get('*', (_, res) => {
+  res.sendFile(path.resolve(__dirname, 'public-flutter/index.html'));
 });
+
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hoang Tung</h1>');
+// });
 
 io.on('connection', (socket) => {
   socket.join("anomynous_group")
