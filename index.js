b8179b369612ffcd59013e26e44150520f18df8f
TungH <tungh@ichiisoft.com>
2023-08-29 23:40:18 +0700

    add path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path'); 
 const cookieParser = require('cookie-parser');
 const http = require('http');
 const server = http.createServer(app);
